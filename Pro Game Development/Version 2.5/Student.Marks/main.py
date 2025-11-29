c9f7f9bdd9e8850ef3c18e420553b670a0affc4f
StarkEclipse <nore_nate@icloud.com>
2025-11-29 20:17:19 +0100

    Add files via upload

--- Pro Game Development/Version 2.5/Student.Marks/main.py
+++ Pro Game Development/Version 2.5/Student.Marks/main.py
@@ -4,20 +4,20 @@ class Student:
         self.marks = marks
 
     def average(self):
-        pass
-
+        return sum(self.marks) / len(self.marks)
+        
 student1 = Student("Tim", [8.2, 9.1, 5.7, 6.7, 8.3, 7.2])
 student2 = Student("Mike", [4.9, 5.6, 8.0, 7.7, 6.2, 5.9])
 
 avg1 = student1.average()
 avg2 = student2.average()
 
-print(f"{student1[1]}'s average: {avg1}")
-print(f"{student2[1]}'s average: {avg2}")
+print(f"{student1.name}'s average: {avg1}")
+print(f"{student2.name}'s average: {avg2}")
 
 if avg1 > avg2:
-    print(f"{student1} scored higher!")
+    print(f"{student1.name} scored higher!")
 elif avg1 < avg2:
-    print(f"{student2} scored higher!")
+    print(f"{student2.name} scored higher!")
 else:
     print("Both students scored the same!!")
